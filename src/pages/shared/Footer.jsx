71edf7d628d75d02827287bd7d070ea9bad155ac
mizanff <mrmizanurrahman626@gmail.com>
2025-08-05 14:50:20 +0600

    added the footer

--- src/pages/shared/Footer.jsx
+++ src/pages/shared/Footer.jsx
@@ -1,49 +1,49 @@
 import React from "react";
-import { FaInstagram, FaLinkedin } from "react-icons/fa";
+import { FaAngleRight, FaInstagram, FaLinkedin } from "react-icons/fa";
 import { IoLogoFacebook } from "react-icons/io";
+// Reusable Link Section Component
+const LinkSection = ({ title = "Important Links", links = [] }) => (
+  <div className="md:pl-10 md:text-lg space-y-2 md:space-y-3">
+    <h3 className="text-[20px] md:text-[24px] font-bold mt-5 md:mt-0">{title}</h3>
+    {links.map((link, idx) => (
+      <div key={idx} className="flex items-center gap-2 hover:text-[#FF6347]">
+        <FaAngleRight />
+        <p>{link}</p>
+      </div>
+    ))}
+  </div>
+);
 
 const Footer = () => {
   return (
-    <div className="roboto-serif-font md:pl-10 px-4 mx-auto py-4 my-12 md:my-16">
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 mx-auto">
-        {/* step-1 */}
-        <div className="">
+    <div className="roboto-serif-font md:pl-10 px-4 mx-auto py-20 mt-8 md:mt-20  bg-[#b0c4de2d] rounded-2xl">
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 mx-auto md:gap-6">
+        {/* Company Info */}
+        <div className="space-y-3 mb-5 md:mb-0">
           <h3 className="md:text-3xl text-2xl font-bold">Car Rental</h3>
           <p className="text-lg">
-            Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Possimus, voluptate.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, voluptate.
           </p>
           <h4 className="text-lg">Noida, Uttar Pradesh</h4>
           <h5 className="text-lg">+91 123456789</h5>
-          <div className="flex">
+          <div className="flex gap-4 text-2xl mt-2">
             <FaInstagram />
             <IoLogoFacebook />
             <FaLinkedin />
           </div>
         </div>
-        {/* step-2 */}
-        <div className="md:pl-20">
-          <h3>Important Links</h3>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-        </div>
-        {/* step-3 */}
-        <div className="md:pl-20">
-          <h3>Important Links</h3>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-        </div>
-        {/* step-4 */}
-        <div className="md:pl-20">
-          <h3>Important Links</h3>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
+
+        {/* Link Sections */}
+        <div className="col-span-3 grid grid-cols-2 md:grid-cols-3 gap-4">
+          <LinkSection
+            links={["Home", "About", "Contact name", "Blog"]}
+          />
+          <LinkSection
+            links={["Home", "About", "Contact", "Blog visit"]}
+          />
+          <LinkSection
+            links={["Home game", "About last", "Contact asd", "Blog"]}
+          />
         </div>
       </div>
     </div>
